20ca80108ed00bef9862206c30cd6dac14c5837d
Igor Krivenko <igor.s.krivenko@gmail.com>
2019-06-08 14:56:42 -0400

    Added energy() method to IsingMC class

--- ising.py
+++ ising.py
@@ -8,11 +8,55 @@ class IsingMC(object):
     np.random.seed(seed)
     self.data = 2*np.random.randint(low = 0, high = 2, size = (Lx,Ly)) - 1
 
+  def energy(self):
+    Lx, Ly = self.data.shape
+    e = 0
+    for i in range(Lx):
+      for j in range(Ly):
+        site = self.data[i,j]
+        neigh = []
+        #Lower neigh
+        if (j == 0):
+          neigh.append(self.data[i,Ly-1])
+        else:
+          neigh.append(self.data[i,j-1])
+        #Upper neigh
+        if (j == Ly-1):
+          neigh.append(self.data[i,0])
+        else:
+          neigh.append(self.data[i,j+1])
+        #Left neigh
+        if (i == 0):
+          neigh.append(self.data[Lx-1,j])
+        else:
+          neigh.append(self.data[i-1,j])
+        #Right neigh
+        if (i == Lx-1):
+          neigh.append(self.data[0,j])
+        else:
+          neigh.append(self.data[i+1,j])
+        for n in neigh:
+          e -= site*n
+        e -= site*self.h
+
+    return e
 
 def test_init():
-    mc = IsingMC(2, 2, 0.1, 10)
+  mc = IsingMC(2, 2, 0.1, 10)
 
-    assert mc.data.shape == (2, 2)
-    assert (np.abs(mc.data) == 1).all()
-    assert mc.h == 0.1
-    assert mc.beta == 10
+  assert mc.data.shape == (2, 2)
+  assert (np.abs(mc.data) == 1).all()
+  assert mc.h == 0.1
+  assert mc.beta == 10
+
+def test_energy():
+  Lx = 10
+  Ly = 10
+  h = 0.1
+  mc = IsingMC(Lx, Ly, h, 2)
+  N = Lx*Ly
+
+  mc.data = np.ones((Lx,Ly), dtype=int)
+  assert np.abs(mc.energy() - (-4*N - h*N)) < 1e-10
+  mc.data = -np.ones((Lx,Ly), dtype=int)
+  assert np.abs(mc.energy() - (-4*N + h*N)) < 1e-10
